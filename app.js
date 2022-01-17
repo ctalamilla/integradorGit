6a7551b9cbb89f6561b978b22db6da56196fa2f5
Cristian Patricio Salinas Talamilla <ctalamilla@MacBook-Pro-de-Cristian.local>
2022-01-17 13:45:06 -0300

    actualizado app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+let contactos = require("./contactos");
 let domicilio = require("./domicilios");
 let usuario = require("./usuario")
+
+
 
 console.log('Soy una aplicacion')
 console.log("===============")
 console.log(domicilio)
+console.log("===============")
 console.log(usuario)
+console.log("===============")
+console.log(contactos)
