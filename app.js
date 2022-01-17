64b5341f49e56ea03bb54c205ea3956db546d56a
Yonibel <gyonibel@gmail.com>
2022-01-17 13:52:38 -0300

    se crea comentario en app

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let usuario = require("./usuario")
 
 
 console.log('Soy una aplicacion')
-console.log("===============")
+console.log("Esto es Domicilio")
 console.log(domicilio)
-console.log("===============")
+console.log("Estos son los Usuarios")
 console.log(usuario)
-console.log("===============")
+console.log("Estos son los contactos")
 console.log(contactos)
